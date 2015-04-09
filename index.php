0e365750e4f0336a5ed3630066af2f92e3e84f06
SuperMex <adelinezzm@gmail.com>
2015-04-09 15:14:10 -0400

    checkpoint commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Project 1 Checkpoint</title>
+</head>
+
+<body>
+<p>Welcome!</p>
+<p><a href="checkpoint.html">Checkpoint</a></p>
+</body>
+
+</html>
