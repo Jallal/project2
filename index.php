9fd20e6e88c6c952e542aacd7986280d00779ab5
elhazzat <jhazzat@gmail.com>
2015-04-10 16:29:41 -0400

    file arrangments

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
-<!doctype html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elhazzat
+ * Date: 2/12/15
+ * Time: 5:03 PM
+ */
+require 'format.inc.php';
+require 'lib/game.inc.php';
+$view = new SudokuView($sudoku);
+?>
+
+<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Project 1 Checkpoint</title>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Sudoku game</title>
+    <link rel="stylesheet" href="Sudoku.css" />
 </head>
 
 <body>
-<p>Welcome!</p>
-<p><a href="checkpoint.html">Checkpoint</a></p>
+
+<h1>Welcome to our Sudoku Game!</h1>
+
+<div class="guess-box">
+
+<br>Enter the player name to start a new game:<br>
+<form   name=username" action="game-post.php?username" method="post">
+    <input type="text" name="name" placeholder="John" value="">
+    <br>
+    <input type="submit"  name ="username" value="Start">
+</form>
+
+    <a href="game-post.php?c">Cheat Mode</a>
+</div>
+
 </body>
 
 </html>
+
